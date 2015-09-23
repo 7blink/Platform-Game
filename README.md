9464e535868586b34e9c6c3b0206a3d349cf9ad7
7blink <7blinkcreations@gmail.com>
2015-09-22 20:15:32 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Platform-Game
+
+This is a simple little platform game that I made about a year ago.  You can use the arrow keys or the on screen keyboard if you are on a touch device.
+
+This game was built using the Quintus framework.  Check out their website [http://www.html5quintus.com/](http://www.html5quintus.com/)
---
+++ scripts/enemy.js
@@ -0,0 +1,63 @@
+Quintus.ActionPlatformerEnemy = function(Q) {
+
+	Q.component("commonEnemy", {
+		added: function() {
+			var entity = this.entity;
+			entity.on("bump.left, bump.right, bump.bottom", function(collision){
+				if(collision.obj.isA("Player")) {
+					collision.obj.damage();
+				}
+				
+			});
+			entity.on("bump.top", function(collision){
+				if(collision.obj.isA("Player")) {
+					collision.obj.p.vy = -100;
+					Q.audio.play('kill-enemy.mp3');
+					this.destroy();
+				}
+			});
+		}
+	});
+
+	Q.Sprite.extend("GroundEnemy", {
+		init:function(p) {
+			this._super(p, {vx: -50, defaultDirection: "left"});
+			this.add("2d, aiBounce, commonEnemy")
+		},
+		step: function(dt) {        
+      var dirX = this.p.vx / Math.abs(this.p.vx);
+      var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+      var nextElement = Q.stage().locate(this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+      var nextTile;
+
+      if(nextElement instanceof Q.TileLayer) {
+        nextTile = true;
+      }
+
+      //if we are on ground and there is a cliff
+      if(!nextTile && ground) {
+        if(this.p.vx > 0) {
+          if(this.p.defaultDirection == "right") {
+            this.p.flip = "x";
+          }
+          else {
+            this.p.flip = false;
+          }
+        }
+        else {
+          if(this.p.defaultDirection == "left") {
+            this.p.flip = "x";
+          }
+          else {
+            this.p.flip = false;
+          }
+        }
+        this.p.vx = -this.p.vx;
+      }
+
+    }
+
+
+	});
+
+};
